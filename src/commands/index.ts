7655cc30ca049eb52b1fad7a6d5614981ab209a9
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2026-04-25 13:05:00 -0500

    add chat commands with the ability to add additional system prompts

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,12 @@
 import ping from "./ping";
 import gd from "./gd";
+import chat from "./chat"
 import type { SlashCommand } from "../types/command";
 
 const commands: SlashCommand[] = [ping, gd];
+
+if (process.env.OPENAI_API_KEY) {
+    commands.push(chat)
+}
 
 export default commands;
